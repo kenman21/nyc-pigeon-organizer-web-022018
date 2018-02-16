c4f70f3248cdd9a078beda676cf7fb97d04c02ff
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-02-16 17:47:51 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,13 +1,21 @@
 def nyc_pigeon_organizer(data)
-  output_hash = Hash.new
   data_hash = Hash.new
-  data_array = Array.new
   data.each do |char, namesanddata|
     namesanddata.each do |data, names|
-      if data_hash[namesanddata].size != 0
-        data_hash[namesanddata].push 
-      else
-        data_hash[namesanddata] = data
-      end 
+      names.each do |name|
+        if data_hash == {}
+          data_hash = {name => {char => [data.to_s]}}
+        elsif data_hash.keys.include?(name)
+          if data_hash[name].keys.include?(char)
+            data_hash[name][char].push(data.to_s)
+          else 
+            data_hash[name][char] = [data.to_s]
+          end 
+        else 
+          data_hash[name] = {char => [data.to_s]}
+        end 
+      end
     end 
+  end
+  data_hash
 end
