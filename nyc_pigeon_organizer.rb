a683a3deb6715df271cbf872313b253e6bee97d1
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-02-16 17:47:47 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  output_hash = Hash.new
+  data_hash = Hash.new
+  data_array = Array.new
+  data.each do |char, namesanddata|
+    namesanddata.each do |data, names|
+      if data_hash[namesanddata].size != 0
+        data_hash[namesanddata].push 
+      else
+        data_hash[namesanddata] = data
+      end 
+    end 
 end
